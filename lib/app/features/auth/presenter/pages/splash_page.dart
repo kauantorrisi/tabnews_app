0b23e3f4880c5468ee7651d21a1e4bc185e83110
Kauan Torrisi <torrisikauan@gmail.com>
2023-02-17 10:27:00 -0300

    adjust the splash_page

--- lib/app/features/auth/presenter/pages/splash_page.dart
+++ lib/app/features/auth/presenter/pages/splash_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:tabnews_app/app/features/auth/services/auth_prefs_service.dart';
 import 'package:tabnews_app/libraries/common/design/app_colors.dart';
@@ -19,10 +20,11 @@ class _SplashPageState extends State<SplashPage> {
       AuthPrefsService().isAuth(),
       Future.delayed(const Duration(seconds: 3)),
     ]).then((value) => value[0]
-        ? Modular.to.pushReplacementNamed('/tabs-module/', arguments: {
+        ? Modular.to.pushNamed(Modular
+            .initialRoute) /* Modular.to.pushReplacementNamed('/tabs-module/', arguments: {
             "token": AuthPrefsService.token,
             "isGuest": AuthPrefsService.isGuest,
-          })
+          }) */
         : Modular.to.pushReplacementNamed('/auth-module/login-page'));
     super.initState();
   }
@@ -33,31 +35,15 @@ class _SplashPageState extends State<SplashPage> {
       child: Scaffold(
         backgroundColor: AppColors.black,
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             const Spacer(),
             Center(
-              child: Column(
-                children: [
-                  Text(
-                    'TabNews',
-                    style: TextStyle(
-                      fontSize: 100,
-                      color: AppColors.white,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1,
-                    ),
-                  ),
-                  Text(
-                    'with Flutter',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: AppColors.white,
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 1,
-                    ),
-                  ),
-                ],
+              child: Image.asset(
+                'lib/assets/images/TabNewsIcon.png',
+                height: 100,
+                fit: BoxFit.fill,
               ),
             ),
             const Spacer(),
